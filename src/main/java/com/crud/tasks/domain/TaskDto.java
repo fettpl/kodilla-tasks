509f418462d6a52e2fdfcbeb407271a656e7167b
fettpl <38704082+fettpl@users.noreply.github.com>
2018-08-28 16:48:21 +0200

    Fix: TaskDto updated with @NoArgsConstructor

--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -1,15 +1,14 @@
 package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class TaskDto {
     private Long id;
     private String title;
     private String content;
-
-    public TaskDto() {
-    }
 }
