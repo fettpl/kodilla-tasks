7cc648db2b09f72a132841d053a2cd4dd8a57ddc
fettpl <pawel.borawski@gmail.com>
2018-09-22 18:34:07 +0200

    Feat: Added TrelloController with jasypt encryption

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,11 +1,17 @@
 package com.crud.tasks;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import com.ulisesbocchio.jasyptspringboot.annotation.EncryptablePropertySource;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEncryptableProperties
+@EncryptablePropertySource(name = "ApplicationProperties", value = "classpath:application.properties")
 //public class TasksApplication extends SpringBootServletInitializer {
 public class TasksApplication {
 	public static void main(String[] args) {
@@ -16,4 +22,21 @@ public class TasksApplication {
 //	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 //		return application.sources(TasksApplication.class);
 //	}
+	@Bean("jasyptStringEncryptor")
+	public StringEncryptor stringEncryptor() {
+		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+		config.setPassword("LoremIpsum88@");
+		config.setAlgorithm("PBEWithMD5AndDES");
+		config.setKeyObtentionIterations("1000");
+		config.setPoolSize("1");
+		config.setProviderName("SunJCE");
+		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+		config.setStringOutputType("base64");
+		encryptor.setConfig(config);
+		return encryptor;
+	}
+
+	String originalKey = stringEncryptor().decrypt("${trello.app.key}");
+	String originalToken = stringEncryptor().decrypt("${trello.app.token}");
 }
