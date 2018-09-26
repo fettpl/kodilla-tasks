7b8eb41eb9bef37a18a2be89a15c25d1ffac3288
fettpl <pawel.borawski@gmail.comgit config --global user.name fettpl>
2018-09-26 19:28:28 +0200

    Fix: Jasypt update

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -3,12 +3,8 @@ package com.crud.tasks;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import com.ulisesbocchio.jasyptspringboot.annotation.EncryptablePropertySource;
 import com.ulisesbocchio.jasyptspringboot.environment.StandardEncryptableEnvironment;
-import org.jasypt.encryption.StringEncryptor;
-import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
-import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
@@ -21,20 +17,5 @@ public class TasksApplication {
 		new SpringApplicationBuilder()
 				.environment(new StandardEncryptableEnvironment())
 				.sources(TasksApplication.class).run(args);
-	}
-
-	@Bean("jasyptStringEncryptor")
-	public StringEncryptor stringEncryptor() {
-		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-		config.setPassword("LoremIpsum88@");
-		config.setAlgorithm("PBEWithMD5AndDES");
-		config.setKeyObtentionIterations("1000");
-		config.setPoolSize("1");
-		config.setProviderName("SunJCE");
-		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
-		config.setStringOutputType("base64");
-		encryptor.setConfig(config);
-		return encryptor;
 	}
 }
