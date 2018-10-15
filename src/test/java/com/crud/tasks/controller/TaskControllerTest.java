8682dc129d400f2f221cfa6174507330106235fc
fettpl <pawel.borawski@gmail.comgit config --global user.name fettpl>
2018-10-15 21:34:22 +0200

    Feat: Added TrelloController TestSuite

---
+++ src/test/java/com/crud/tasks/controller/TaskControllerTest.java
@@ -0,0 +1,11 @@
+package com.crud.tasks.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(TrelloController.class)
+public class TaskControllerTest {
+
+}
