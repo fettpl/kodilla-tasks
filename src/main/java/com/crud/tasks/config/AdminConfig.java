f9ee60bc760720722ba0413f67dc99aad05ef836
fettpl <pawel.borawski@gmail.comgit config --global user.name fettpl>
2018-10-17 00:55:27 +0200

    Feat: Added StaticWebPageController

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Component;
 @Component
 @Getter
 public class AdminConfig {
-    @Value("${admin.mail}")
+    @Value("${admin.mail")
     private String adminMail;
+
+    @Value("${admin.name}")
+    private String adminName;
 }
