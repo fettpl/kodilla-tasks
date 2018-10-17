ceabcd92ffbb6a9783776298932a0188bd5667eb
fettpl <pawel.borawski@gmail.comgit config --global user.name fettpl>
2018-10-17 10:11:47 +0200

    Fix: Updated AdminConfig and MailCreatorService

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 @Getter
 public class AdminConfig {
-    @Value("${admin.mail")
+    @Value("${admin.mail}")
     private String adminMail;
 
     @Value("${admin.name}")
--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -9,6 +9,9 @@ import org.thymeleaf.context.Context;
 
 @Service
 public class MailCreatorService {
+    private static final String BUTTON = "Visit website";
+    private static final String GOODBYE = "This notification was generated automatically. Please do not respond. Thanks for your cooperation!";
+    private static final String URL = "http://localhost:8888/crud";
 
     @Autowired
     private AdminConfig adminConfig;
@@ -20,10 +23,10 @@ public class MailCreatorService {
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
-        context.setVariable("tasks_url", "http://localhost:8888/crud");
-        context.setVariable("button", "Visit website");
+        context.setVariable("tasks_url", URL);
+        context.setVariable("button", BUTTON);
         context.setVariable("admin_name", adminConfig.getAdminName());
-        context.setVariable("goodbye_message", "This notification was generated automatically. Please do not respond. Thanks for your cooperation!");
+        context.setVariable("goodbye_message", GOODBYE);
         context.setVariable("company_details", adminConfig.getAdminMail());
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
